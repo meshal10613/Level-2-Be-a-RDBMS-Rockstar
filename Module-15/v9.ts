299b02a3b9439c61259ee8eda72c998114d8c2ff
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-12-10 18:07:34 +0600

    module 15, vedio 9 added

---
+++ Module-15/v9.ts
@@ -0,0 +1,25 @@
+/**
+ ** Software Development Lifecycle
+ * 
+ * 1. Planning
+ * 2. Analysis of Requirements
+ * 3. System Design
+ * 4. Implementation
+ * 5. Testing & Integration
+ * 6. Maintainance
+ */
+
+/**
+ ** Purpose of Database Design
+ * 
+ * Structured organization for efficient data management & retrieval
+*/
+
+/**
+ ** Relationship Cardinality
+ * 
+ * 1. One to One
+ * 2. One to Many
+ * 3. Many to Many
+ * 4. Many to One
+ */
