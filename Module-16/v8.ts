da7c52e0c4df96f845f98909055c9561ef6852ac
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-12-12 22:11:43 +0600

    module 16, vedio 8, done

---
+++ Module-16/v8.ts
@@ -0,0 +1,10 @@
+/**
+ *? PostgreSQL
+ ** --> The world's most advance open source Relational Database Management System(RDBMS)
+ * 1. Open Source
+ * 2. Scalability
+ * 3. Modern
+ * 4. Advance Data Type
+ * 5. ACID Compliance
+ * 6. Indexing
+ */
