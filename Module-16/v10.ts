62209d64147b737ea43dab181945b91066aa641e
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-12-13 10:36:51 +0600

    module 16 done

---
+++ Module-16/v10.ts
@@ -0,0 +1,12 @@
+/**
+ ** Some PostgreSQL Commands- in psql shell
+ * 
+ * ? list of database-> \l
+ * ? list of tables-> \dt
+ * ? list of columns-> \d
+ * ? list of connection information-> \conninfo
+ * ? list of roles-> \du
+ * ? list of all commands-> \?
+ * ? for quit-> \q
+ * ? check version-> select version();
+ */
